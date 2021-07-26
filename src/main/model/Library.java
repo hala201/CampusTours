aff5c7d59058785e7126fcf03207f69258fbb95e
hala201 <halamurad2000@hotmail.com>
2021-07-26 15:40:22 -0700

    More test coverage, console documentation

--- src/main/model/Library.java
+++ src/main/model/Library.java
@@ -6,6 +6,7 @@ public class Library extends TourStop {
     public Library(String name, String location) {
         super(name, location);
     }
+
     //EFFECTS: returns the tour stop type "Library"
     @Override
     public String getTourStopType() {
